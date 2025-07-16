b0448a9a436d678f54b2023aa9933a8910501a40
ramvikash <vikashkarthik133@gmail.com>
2025-07-16 13:44:26 +0530

    puppeteer is added to index and package.json file

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const scraperRoutes = require('./routes/scraperRoutes');
+const puppeteer = required('puppeteer');
 
 const app = express();
+const browser = await puppeteer.launch({
+  headless: true,
+  executablePath: puppeteer.executablePath(), // ✅ dynamically uses installed Chrome
+  args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
 app.use(cors());
 app.use(express.json());
 app.use('/api/scrape', scraperRoutes);
+
 
 const PORT = process.env.PORT ||4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
