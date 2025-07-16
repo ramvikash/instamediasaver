cf718cff68d8ad9b187195c176d6f3bbb74b871e
ramvikash <vikashkarthik133@gmail.com>
2025-07-16 13:34:29 +0530

    port value changet to 4000

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/scrape', scraperRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ||4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
