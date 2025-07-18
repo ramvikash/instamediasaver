fd5eb7b7be37c3cc9188592e57405914e7f0e1bd
ramvikash <vikashkarthik133@gmail.com>
2025-07-18 14:27:53 +0530

    added puppeteer core

--- index.mjs
+++ index.mjs
@@ -6,18 +6,22 @@ import express from 'express';
 import cors from 'cors';
 import scraperRoutes from './routes/scraperRoutes.js';
 import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
 
 const app = express();
 const browser = await puppeteer.launch({
-  headless: true,
-  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium', // or /usr/bin/chromium
-  //executablePath: puppeteer.executablePath(), // ✅ dynamically uses installed Chrome
-  args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
 });
+// const browser = await puppeteer.launch({
+//   headless: true,
+//   executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium', // or /usr/bin/chromium
+//   //executablePath: puppeteer.executablePath(), // ✅ dynamically uses installed Chrome
+//   args: ['--no-sandbox', '--disable-setuid-sandbox']
+// });
 app.use(cors());
 app.use(express.json());
 app.use('/api/scrape', scraperRoutes);
 
 
-const PORT = process.env.PORT ||4000;
+const PORT = process.env.PORT ||5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
