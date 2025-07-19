ac070f6a57db4edfd6bdf5caf714d61a116990a2
ramvikash <68604155+ramvikash@users.noreply.github.com>
2025-07-19 23:26:49 +0530

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -13,6 +13,6 @@ const PORT = process.env.PORT ||5000;
 
 app.use(cors());
 app.use(express.json());
-app.get('/api/scrape', scraperRoutes);
+app.post('/api/scrape', scraperRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
